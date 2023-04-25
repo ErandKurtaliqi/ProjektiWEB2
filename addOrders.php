e51e4a7d486cb56df984aa9f8053d651cf01a2ac
Erand12334 <erandkurtaliqi1@gmail.com>
2023-04-25 23:22:19 +0200

    css

--- addOrders.php
+++ addOrders.php
@@ -4,28 +4,38 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="addOrders.css">
     <title>Add Orders</title>
 </head>
 <body>
-
-<form action="addOrders-php.php" method="post"><br><br>
-<label for="id_orders">Shto id e orders</label><br>
-    <input type="number" placeholder="id_orders" id="id_orders" name="id_orders"><br><br>
-    <label for="id_booking">Shto id e booking</label><br>
-    <input type="number" placeholder="id_booking" id="id_booking" name="id_booking"><br><br>
-    <label for="id_user_reg">Shto id e perdoruesit</label><br>
-    <input type="number" placeholder="id_user_reg" id="id_user_reg" name="id_user_reg"><br><br>
-    <label for="id_countries">Shto id e countries</label><br>
-    <input type="number" placeholder="id_countries" id="id_countries" name="id_countries"><br><br>
-    <label for="id_hotels">Shto id e hotels</label><br>
-    <input type="number" placeholder="id_hotels" id="id_hotels" name="id_hotels"><br><br>
-    <label for="id_company_fly">Shto id e company_fly</label><br>
-    <input type="number" placeholder="id_company_fly" id="id_company_fly" name="id_company_fly"><br><br>
-    <label for="user_name">Shto user_name</label><br>
-    <input type="text" placeholder="user_name" id="user_name" name="user_name"><br><br>
+<div class="form-container">
+<div class="form-inner">
+<form action="addOrders-php.php" method="post" class="login"><br><br>
+<div class="field">
+    <input type="number" placeholder="Shto id e orders" id="id_orders" name="id_orders"><br><br>
+</div>
+    <div class="field">
+    <input type="number" placeholder="Shto id e booking" id="id_booking" name="id_booking" ><br><br>
+    </div>
+    <div class="field">
+    <input type="number" placeholder="Shto id e perdoruesit" id="id_user_reg" name="id_user_reg"><br><br>
+    </div>
+    <div class="field">
+    <input type="number" placeholder="Shto id e countries" id="id_countries" name="id_countries"><br><br>
+    </div>
+    <div class="field">
+    <input type="number" placeholder="Shto id e hotels" id="id_hotels" name="id_hotels"><br><br>
+    </div>
+    <div class="field">
+    <input type="number" placeholder="Shto id e company_fly" id="id_company_fly" name="id_company_fly"><br><br>
+    </div>
+    <div class="field">
+    <input type="text" placeholder="Shto user_name" id="user_name" name="user_name"><br><br>
+    </div>
     <button type="submit" class="btn">Add</button><br><br>
 </form>
+</div>
+</div>
 
 <button type="button" onclick="document.getElementById('tabela1').style.display='block'" class="btn"
 >Booking Table</button>
@@ -33,9 +43,12 @@
 >Orders Table</button>
 <button type="button" onclick="document.getElementById('tabela3').style.display='block'" class="btn"
 >Hotels Table</button>
-<button type="button" onclick="document.getElementById('tabela4').style.display='block'">Countries Table</button>
-<button type="button" onclick="document.getElementById('tabela5').style.display='block'">User Table</button>
-<button type="button" onclick="document.getElementById('tabela6').style.display='block'">Company_Fly Table</button>
+<button type="button" onclick="document.getElementById('tabela4').style.display='block'" class="btn"
+>Countries Table</button>
+<button type="button" onclick="document.getElementById('tabela5').style.display='block'" class="btn"
+>User Table</button>
+<button type="button" onclick="document.getElementById('tabela6').style.display='block'" class="btn"
+>Company_Fly Table</button>
 
 <?php
 
