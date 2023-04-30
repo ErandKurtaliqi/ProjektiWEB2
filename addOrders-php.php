0baf219a592be83e8d005031b0a41196dd16df98
Erand12334 <erandkurtaliqi1@gmail.com>
2023-04-30 22:15:28 +0200

    Price for Pay

--- addOrders-php.php
+++ addOrders-php.php
@@ -49,7 +49,7 @@ if (isset($_POST['id_orders']) && isset($_POST['id_booking'])
         echo "User_name is required";
 	    exit();
 	}else{
-		
+
 		$sql2 = "INSERT INTO orders (id_orders, id_booking, id_user_reg, id_countries, id_hotels, id_company_fly, user_name)
          VALUES('$id_orders', '$id_booking', '$id_user_reg', '$id_countries', '$id_hotels', '$id_company_fly', '$user_name')";
            $result2 = mysqli_query($conn, $sql2);
@@ -63,61 +63,10 @@ if (isset($_POST['id_orders']) && isset($_POST['id_booking'])
 		}
     }
 
-
-$sql = "SELECT * FROM orders 
-        INNER JOIN booking ON orders.id_booking = booking.id_booking
-        INNER JOIN user_reg ON orders.id_user_reg = user_reg.id_user_reg
-        INNER JOIN countries ON orders.id_countries = countries.id_countries
-        INNER JOIN hotels ON hotels.id_hotels = countries.id_hotels
-        INNER JOIN company_fly ON company_fly.id_company_fly = countries.id_company_fly
-		WHERE '$username' = orders.user_name";
-        
-$result = mysqli_query($conn, $sql);
-
-if (mysqli_num_rows($result) > 0) {
-  
-	echo "<table border = 1>
-	<tr><th>ID</th>
-	<th>Booking ID</th>
-	<th>User ID</th>
-	<th>Country ID</th>
-	<th>Name</th>
-	<th>Email</th>
-	<th>Phone</th>
-	<th>Address</th>
-	<th>Location</th>
-	<th>Guests</th>
-	<th>Arrivals</th>
-	<th>Leaving</th>
-	<th>Hotel Name</th>
-	<th>Company Name</th>
-	<th>Name user</th>
-	<th>Price</th>";
-    while($row = mysqli_fetch_assoc($result)) {
-        echo "<tr><td>" . $row["id_orders"] . "</td>
-		<td>".$row["id_booking"]."</td>
-		<td>".$row["id_user_reg"]."</td>
-		<td>".$row["id_countries"]."</td>
-		<td>".$row["names"]."</td>
-		<td>".$row["email"]."</td>
-		<td>".$row["phone"]."</td>
-		<td>".$row["address"]."</td>
-		<td>".$row["location"]."</td>
-		<td>".$row["guests"]."</td>
-		<td>".$row["arrivals"]."</td>
-		<td>".$row["leaving"]."</td>
-		<td>".$row["name_hotels"]."</td>
-		<td>".$row["name_company_fly"]."</td>
-		<td>".$row["name_user_reg"]."</td>
-		<td>".$row["price_countries"]."</td></tr>";
-    }
-    echo "</table>";
-} else {
+else {
     echo "Nuk u gjet asnje rezultat.";
 }
 
-
 mysqli_close($conn);
-
 
 ?>
--- book_form_php.php
+++ book_form_php.php
@@ -85,45 +85,7 @@ if (isset($_POST['names']) && isset($_POST['email'])
         header("Location: book_form.php?error=Leaving is required&$user_data");
 	    exit();
 	}
-
-// 		$sql = "SELECT 
-//     booking.guests,  
-//     countries.price_countries, 
-//     (countries.price_countries * booking.guests) AS total_price
-// FROM 
-//     orders 
-//     JOIN booking ON orders.id_booking = booking.id_booking
-//     JOIN hotels ON orders.id_hotels = hotels.id_hotels
-//     JOIN countries ON orders.id_countries = countries.id_countries
-//     JOIN company_fly ON orders.id_company_fly = company_fly.id_company_fly
-// WHERE 
-//      booking.id_booking = orders.id_booking";
-
-
-// 	$result = mysqli_query($conn, $sql);
-
-	// if (mysqli_num_rows($result) > 0) {
-	// 	// Shfaqni të dhënat për secilën rreshtë
-	// 	while($row = mysqli_fetch_assoc($result)) {
-		
-	// 		// echo "<script> alert('Numri i mysafirëve: " . $row["guests"]. 
-	// 			 ", Çmimi i vendit të destinacionit: " . $row["price_countries"]. 
-	// 			 ", Çmimi total: " . $row["total_price"]."<br>');</script>";
-	// 	}
-	// }
-	// else if(strlen($leaving) <= strlen($arrivals)){
-	// 	header("Location: book_form.php?error=Leaving is impossible&$user_data");
-	//     exit();
-	// }
-
-	// else{
-
-	//     $sql = "SELECT * FROM book_form1 WHERE names='$names' ";
-	// 	$result = mysqli_query($conn, $sql);
-
-	// 	if (mysqli_num_rows($result) > 0) {
-	// 		header("Location: book_form.php?error=The names is taken try another&$user_data");
-	//         exit();
+	
 	else {
            $sql2 = "INSERT INTO booking(names, email, phone, address, location, guests, arrivals, leaving)
 		            VALUES('$names', '$email', '$phone', '$address', '$location', '$guests', '$arrivals', '$leaving')";
