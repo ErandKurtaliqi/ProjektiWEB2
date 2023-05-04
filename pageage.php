d829a87883d56bfb1eb1a60c4d4ab63ce3dd6313
Erand12334 <erandkurtaliqi1@gmail.com>
2023-05-04 11:40:46 +0200

    Final

--- pageage.php
+++ pageage.php
@@ -39,6 +39,8 @@
 
 <section class="packages">
     <h1 class="heading-title">top destinations</h1>
+    <p style="color: #8e44ad; font-size: 15px; margin-left: 100px;">If you want to see the current weather for the place you wish to visit, you can click on the "Weather" button.</p>
+    <a href="weather.html" class="btn" style="margin-bottom: 20px; margin-left: 460px;">Weather</a>
     <div class="box-container">
         <div class="box">
             <div class="image">
