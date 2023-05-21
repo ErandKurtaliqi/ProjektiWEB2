5bd259f839b68dd031903fea239a4e3e3331a558
festinam <festinamjeku1@gmail.com>
2023-05-21 20:52:17 +0200

    Private file

---
+++ private.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>HOME</title> <!--Font awesome cdn link-->
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"/>
+   <link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+   <link rel = "stylesheet" href = "style.css">
+</head>
+<body>
+   <!-- header section starts -->
+   <section class="header">
+
+   <a href = "home.php" class = "logo">TRAVEL</a>
+
+   <nav class = "navbar">
+       <a href = "home.php">HOME</a>
+       <a href = "about.php">ABOUT</a>
+       <a href = "pageage.php">PACKAGE</a>
+       <a href = "book_form.php">BOOK</a>
+       <a href="contact.php">CONTACT</a>
+   </nav>
+   <nav class = "navbar23">
+   <a href = "login.php" >Login</a>
+   </nav>
+
+   <div id = "menu-btn" class = "fas fa-bars"></div>
+
+
+
+   </section>
+
+   <h1></h1>
+</body>
+</html>
